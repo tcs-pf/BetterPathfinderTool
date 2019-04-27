6f1d79d4f570ee9dbacf33e3eaed2fc6c7658805
Matthew Fritsch <matthewmfritsch@gmail.com>
2019-04-26 21:34:57 -0700

    Adding subfolders for every section for organization

---
+++ Webscraper/FeatExport/spider.py
@@ -0,0 +1,59 @@
+import scrapy, os
+
+class RaceSpider(scrapy.Spider):
+    name = "RaceSpider"
+    download_delay = 10
+    start_urls=[
+        "https://d20pfsrd.com/classes/core-classes/cleric"
+    ]
+
+    def parse(self, response):
+        pageparts = response.url.split("/")
+        pagetitle = pageparts[-1]
+        if(len(pagetitle) < 3):
+            pagetitle = pageparts[-2]
+        filename = "races-" + pagetitle
+        completeName = os.path.join("./Exports/", filename)
+        with open("export-%s.txt"%completeName, "w") as file:
+            file.write(response.url + "\n")
+            for i in range(4):
+                headers = response.css("h%s::text"%(i+1)).extract()
+                for line in headers:
+                    if not "OGN" in line and len(line) > 3:
+                        file.write("h%s - "%(i+1)+line+"\n") 
+            for line in response.css("p ::text").extract(): 
+                if "\n" not in line and len(line) > 2: 
+                    file.write("p - "+line+"\n")
+        resp = response.css("li > a::attr(href)").extract()
+        for line in resp:
+            if "http://www.d20pfsrd.com/races" in line and not pagetitle in line and len(line.split("/")) == 7:
+                self.log("***Working link at %s"%line)    
+                try:
+                    pageparts = response.url.split("/")
+                    spot = -1
+                    if(len(pagetitle) < 3):
+                        spots=-2
+                    filename = "classes-" + pageparts[spots]
+                    f = open("export-%s.txt"%filename, "r")
+                    self.log("This file %s already exists!"%filename)
+                except:
+                    yield response.follow(line, callback=self.parse_second)
+
+            
+    def parse_second(self, response):
+        pageparts = response.url.split("/")
+        pagetitle = pageparts[-1]
+        if(len(pagetitle) < 3):
+            pagetitle = pageparts[-2]
+        filename = "races-" + pagetitle
+        completeName = os.path.join("./Exports/", filename)         
+        with open("export-%s.txt"%completeName, "w") as file:
+            file.write(response.url + "\n")
+            for i in range(4):
+                headers = response.css("h%s::text"%(i+1)).extract()
+                for line in headers:
+                    if not "OGN" in line and len(line) > 3:
+                        file.write("h%s - "%(i+1)+line+"\n") 
+            for line in response.css("p ::text").extract(): 
+                if "\n" not in line and len(line) > 2: 
+                    file.write("p - "+line+"\n")
